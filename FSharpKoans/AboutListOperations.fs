5d2e810c34de9768f6babf169ad6c36d2f77bbbb
Mantone Malikhetla <madikgetlamantone@gmail.com>
2020-05-18 12:56:09 +0200

    remaining sections complete

--- FSharpKoans/AboutListOperations.fs
+++ FSharpKoans/AboutListOperations.fs
@@ -29,7 +29,11 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``01 Finding the length of a list, the hard way`` () =
         let length (xs : 'a list) : int =
-            __ // write a function to find the length of a list
+             let rec  count xs iter = // write a function to find the length of a list
+                 match xs with 
+                 | [] -> iter
+                 | _::rest -> count (rest) (iter+1)
+             count xs 0
         length [9;8;7] |> should equal 3
         length [] |> should equal 0
         length ["Le Comte de Monte-Cristo"] |> should equal 1
@@ -38,12 +42,16 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     // Hint: https://msdn.microsoft.com/en-us/library/ee340354.aspx
     [<Test>]
     let ``02 Finding the length of a list, the easy way`` () =
-        __ [9;8;5;8;45] |> should equal 5
-
+        List.length [9;8;5;8;45] |> should equal 5
+   
     [<Test>]
     let ``03 Reversing a list, the hard way`` () =
         let rev (xs : 'a list) : 'a list =
-            __ // write a function to reverse a list here.
+            let rec shift xs newLst =   // write a function to reverse a list here.
+                match xs with 
+                | [] ->  newLst
+                | a::rest -> shift(rest) (a::newLst)
+            shift xs []
         rev [9;8;7] |> should equal [7;8;9]
         rev [] |> should equal []
         rev [0] |> should equal [0]
@@ -52,15 +60,19 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     // Hint: https://msdn.microsoft.com/en-us/library/ee340277.aspx
     [<Test>]
     let ``04 Reversing a list, the easy way`` () =
-        __ [9;8;7] |> should equal [7;8;9]
-        __ [] |> should equal []
-        __ [0] |> should equal [0]
-        __ [9;8;5;8;45] |> should equal [45;8;5;8;9]
+        List.rev [9;8;7] |> should equal [7;8;9]
+        List.rev [] |> should equal []
+        List.rev [0] |> should equal [0]
+        List.rev [9;8;5;8;45] |> should equal [45;8;5;8;9]
 
     [<Test>]
     let ``05 Fixed-function mapping, the hard way (part 1).`` () =
         let map (xs : int list) : int list =
-            __ // write a function which adds 1 to each element
+            let rec change xs mapped=  // write a function which adds 1 to each element
+               match xs with 
+               | [] -> mapped
+               | a::rest -> change(rest) (a+1::mapped)
+            List.rev(change xs [])
         map [1; 2; 3; 4] |> should equal [2; 3; 4; 5]
         map [9; 8; 7; 6] |> should equal [10; 9; 8; 7]
         map [15; 2; 7] |> should equal [16; 3; 8]
@@ -69,8 +81,13 @@ module ``12: List operations are so easy, you could make them yourself!`` =
 
     [<Test>]
     let ``06 Fixed-function mapping, the hard way (part 2).`` () =
+
         let map (xs : int list) : int list =
-            __ // write a function which doubles each element
+            let rec double xs num = 
+               match xs with 
+               |[] -> num
+               |a:: rest -> double (rest) (a+a::num)
+            List.rev(double xs [])
         map [1; 2; 3; 4] |> should equal [2; 4; 6; 8]
         map [9; 8; 7; 6] |> should equal [18; 16; 14; 12]
         map [15; 2; 7] |> should equal [30; 4; 14]
@@ -89,7 +106,11 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``07 Specified-function mapping, the hard way`` () =
         let map (f : 'a -> 'b) (xs : 'a list) : 'b list =
-            __ // write a map which applies f to each element
+            let rec incrme1 f xs lst = // write a map which applies f to each element
+              match xs with 
+              | [] -> lst
+              | a::rest -> incrme1 f  (rest) (f a::lst)
+            List.rev(incrme1 f xs [])
         map (fun x -> x+1) [9;8;7] |> should equal [10;9;8]
         map ((*) 2) [9;8;7] |> should equal [18;16;14]
         map (fun x -> sprintf "%.2f wut?" x)  [9.3; 1.22] |> should equal ["9.30 wut?"; "1.22 wut?"]
@@ -97,14 +118,21 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     // Hint: https://msdn.microsoft.com/en-us/library/ee370378.aspx
     [<Test>]
     let ``08 Specified-function mapping, the easy way`` () =
-        __ (fun x -> x+1) [9;8;7] |> should equal [10;9;8]
-        __ ((*) 2) [9;8;7] |> should equal [18;16;14]
-        __ (fun x -> sprintf "%.2f wut?" x)  [9.3; 1.22] |> should equal ["9.30 wut?"; "1.22 wut?"]
+        List.map(fun x -> x+1) [9;8;7] |> should equal [10;9;8]
+        List.map((*) 2) [9;8;7] |> should equal [18;16;14]
+        List.map(fun x -> sprintf "%.2f wut?" x)  [9.3; 1.22] |> should equal ["9.30 wut?"; "1.22 wut?"]
 
     [<Test>]
     let ``09 Specified-function filtering, the hard way`` () =
         let filter (f : 'a -> bool) (xs : 'a list) : 'a list =
-            __ // write a function which filters based on the specified criteria
+            let rec findSpec f xs newList = // write a function which filters based on the specified criteria
+               match xs with 
+               | [] -> newList 
+               | a::rest -> 
+                   match f a with
+                   |true -> findSpec f rest (a::newList)
+                   |_ -> findSpec f rest newList
+            List.rev(findSpec f xs [])
         filter (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
         filter (fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
         |> should equal ["woof"; "nyan"; "meow"]
@@ -113,15 +141,22 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     // Hint: https://msdn.microsoft.com/en-us/library/ee370294.aspx
     [<Test>]
     let ``10 Specified-function filtering, the easy way`` () =
-        __ (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
-        __ (fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
+        List.filter(fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
+        List.filter(fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
         |> should equal ["woof"; "nyan"; "meow"]
-        __ (fun (a,b) -> a*b >= 14) [9,3; 4,2; 4,5] |> should equal [9,3; 4,5]
+        List.filter(fun (a,b) -> a*b >= 14) [9,3; 4,2; 4,5] |> should equal [9,3; 4,5]
 
     [<Test>]
     let ``11 Fixed-function filtering, the hard way`` () =
         let filter (xs : int list) : int list =
-            __ // write a function to filter for odd elements only.
+            let rec findOdd xs anotherLst = // write a function to filter for odd elements only.
+              match xs with 
+              | [] -> anotherLst
+              | a::rest -> 
+                  match a % 2 = 1 with 
+                  | true -> findOdd rest (a::anotherLst)
+                  | _ -> findOdd rest anotherLst
+            List.rev(findOdd xs [])
         filter [1; 2; 3; 4] |> should equal [1; 3]
         filter [10; 9; 8; 7] |> should equal [9; 7]
         filter [15; 2; 7] |> should equal [15; 7]
@@ -139,7 +174,14 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     [<Test>]
     let ``12 Specified-function filtering, the hard way`` () =
         let filter (f : 'a -> bool) (xs : 'a list) : 'a list =
-            __ // write a function which filters based on the specified criteria
+            let rec speciFilter f xs newList = // write a function which filters based on the specified criteria
+               match xs with 
+               | [] -> newList
+               | a::rest -> 
+                   match f a with 
+                   |true -> speciFilter f rest (a::newList)
+                   | _ -> speciFilter f rest newList
+            List.rev(speciFilter f xs [])
         filter (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
         filter (fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
         |> should equal ["woof"; "nyan"; "meow"]
@@ -148,10 +190,10 @@ module ``12: List operations are so easy, you could make them yourself!`` =
     // Hint: https://msdn.microsoft.com/en-us/library/ee370294.aspx
     [<Test>]
     let ``13 Specified-function filtering, the easy way`` () =
-        __ (fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
-        __ (fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
+        List.filter(fun x -> x > 19) [9; 5; 23; 66; 4] |> should equal [23; 66]
+        List.filter(fun x -> String.length x = 4) ["moo"; "woof"; "yip"; "nyan"; "meow"]
         |> should equal ["woof"; "nyan"; "meow"]
-        __ (fun (a,b) -> a*b >= 14) [9,3; 4,2; 4,5] |> should equal [9,3; 4,5]
+        List.filter(fun (a,b) -> a*b >= 14) [9,3; 4,2; 4,5] |> should equal [9,3; 4,5]
 
 (*
 A 'fold' starts from a specified state, and generates more states depending
@@ -186,18 +228,26 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``14 A fold which sums a list`` () =
         let fold initialState xs =
-            __ // write a function to do what's described above
+            let rec addList n xs =           // write a function to do what's described above
+              match xs with 
+              | [] -> n
+              | a::rest -> addList (n+a) rest
+            addList initialState xs  
         fold 0 [1; 2; 3; 4] |> should equal 10
         fold 100 [2;4;6;8] |> should equal 120
 
     [<Test>]
     let ``15 A fold which multiplies a list`` () =
         let fold initialState xs =
-            __ // write a function to multiply the elements of a list
-        fold __ [99] |> should equal 99
-        fold 2 [__] |> should equal 22
-        fold __ [1;3;5;7] |> should equal 105
-        fold __ [2;5;3] |> should equal 0
+            let rec multList n xs =              // write a function to multiply the elements of a list
+              match xs with 
+              | [] -> n
+              | a::rest -> multList (n*a) rest 
+            multList initialState xs
+        fold 1 [99] |> should equal 99
+        fold 2 [11] |> should equal 22
+        fold 1 [1;3;5;7] |> should equal 105
+        fold 0 [2;5;3] |> should equal 0
 
     // you probably know the drill by now.  It'd be good to have
     // a function which does the state-generation stuff, wouldn't
@@ -206,27 +256,39 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``16 Folding, the hard way`` () =
         let fold (f : 'a -> 'b -> 'a) (initialState : 'a) (xs : 'b list) : 'a =
-            __  // write a function to do a fold.
+            let rec functionFold f lastVal xs  =  // write a function to do a fold.
+              match xs with 
+              | [] -> lastVal
+              | a::rest -> functionFold f (f lastVal a) rest 
+            functionFold f initialState xs
         fold (+) 0 [1;2;3;4] |> should equal 10
         fold (*) 2 [1;2;3;4] |> should equal 48
         fold (fun state item -> sprintf "%s %s" state item) "items:" ["dog"; "cat"; "bat"; "rat"]
         |> should equal "items: dog cat bat rat"
         fold (fun state item -> state + float item + 0.5) 0.8 [1;3;5;7] |> should equal 18.8
+        //ASK ABOUT THIS!!!!//fold (fun state item -> state + float item + 0.5) 0.8 [1;3;5;7] |> should equal 18.8
 
     // Hint: https://msdn.microsoft.com/en-us/library/ee353894.aspx
     [<Test>]
     let ``17 Folding, the easy way`` () =
-        __ (+) 0 [1;2;3;4] |> should equal 10
-        __ (*) 2 [1;2;3;4] |> should equal 48
-        __ (fun state item -> sprintf "%s %s" state item) "items:" ["dog"; "cat"; "bat"; "rat"]
+        List.fold (+) 0 [1;2;3;4] |> should equal 10
+        List.fold (*) 2 [1;2;3;4] |> should equal 48
+        List.fold (fun state item -> sprintf "%s %s" state item) "items:" ["dog"; "cat"; "bat"; "rat"]
         |> should equal "items: dog cat bat rat"
-        __ (fun state item -> state + float item + 0.5) 0.8 [1;3;5;7] |> should equal 18.8
+        List.fold (fun state item -> state + float item + 0.5) 0.8 [1;3;5;7] |> should equal 18.8
 
     // List.exists
     [<Test>]
     let ``18 exists: finding whether any matching item exists`` () =
         let exists (f : 'a -> bool) (xs : 'a list) : bool =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee370309.aspx
+           let rec check f xs resul = // Does this: https://msdn.microsoft.com/en-us/library/ee370309.aspx
+               match xs with 
+               | [] -> resul
+               | a::rest -> 
+                     match f a with
+                     | true -> true 
+                     | _ -> check f rest (f a)
+           check f xs false
         exists ((=) 4) [7;6;5;4;5] |> should equal true
         exists (fun x -> String.length x < 4) ["true"; "false"] |> should equal false
         exists (fun _ -> true) [] |> should equal false
@@ -234,23 +296,36 @@ or something else), it's likely that you'll be able to use a fold.
     // List.partition
     [<Test>]
     let ``19 partition: splitting a list based on a criterion`` () =
-        let partition (f : 'a -> bool) (xs : 'a list) : ('a list) * ('a list) =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353782.aspx
-        let a, b = partition (fun x -> x%2=0) [1;2;3;4;5;6;7;8;9;10]
-        a |> should equal [2;4;6;8;10]
-        b |> should equal [1;3;5;7;9]
-        let c, d = partition (fun x -> String.length x < 4) ["woof"; "yip"; "moo"; "nyan"; "arf"]
-        c |> should equal ["yip"; "moo"; "arf"]
-        d |> should equal ["woof"; "nyan"]
-        let e, f = partition (fun _ -> false) [9.2; 7.3; 11.8]
-        e |> should equal []
-        f |> should equal [9.2; 7.3; 11.8]
+       let partition (f : 'a -> bool) (xs : 'a list) : ('a list) * ('a list) =
+         // jfvnfjvfj
+           // Does this: https://msdn.microsoft.com/en-us/library/ee353782.aspx
+          let rec separate f xs aList bList = 
+             match xs with 
+             | [] -> (List.rev(aList), List.rev(bList))
+             | x::rest -> 
+                     match f x with 
+                     | true -> separate (f) (rest) (x::aList) (bList) 
+                     | false -> separate (f) (rest) (aList) (x::bList)
+          separate f xs [] [] 
+       let a, b = partition (fun x -> x%2=0) [1;2;3;4;5;6;7;8;9;10]
+       a |> should equal [2;4;6;8;10]
+       b |> should equal [1;3;5;7;9]
+       let c, d = partition (fun x -> String.length x < 4) ["woof"; "yip"; "moo"; "nyan"; "arf"]
+       c |> should equal ["yip"; "moo"; "arf"]
+       d |> should equal ["woof"; "nyan"]
+       let e, f = partition (fun _ -> false) [9.2; 7.3; 11.8]
+       e |> should equal []
+       f |> should equal [9.2; 7.3; 11.8]
 
     // List.init
     [<Test>]
     let ``20 init: creating a list based on a size and a function`` () =
         let init (n : int) (f : int -> 'a) : 'a list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee370497.aspx
+            let rec make  n f lst =  // Does this: https://msdn.microsoft.com/en-us/library/ee370497.aspx
+               match n = 0 with 
+               | true -> lst 
+               | false -> make (n-1) f (f (n-1)::lst)
+            make n f []
         init 10 (fun x -> x*2) |> should equal [0;2;4;6;8;10;12;14;16;18]
         init 4 (sprintf "(%d)") |> should equal ["(0)";"(1)";"(2)";"(3)"]
 
@@ -258,7 +333,14 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``21 tryFind: find the first matching element, if any`` () =
         let tryFind (p : 'a -> bool) (xs : 'a list) : 'a option =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353506.aspx
+            let rec lookFor p xs =  // Does this: https://msdn.microsoft.com/en-us/library/ee353506.aspx
+              match xs with 
+              | [] -> None
+              | a::rest -> 
+                     match p a with 
+                     | true -> Some a
+                     | false -> lookFor p rest
+            lookFor p xs 
         tryFind (fun x -> x<=45) [100;85;25;55;6] |> should equal (Some 25)
         tryFind (fun x -> x>450) [100;85;25;55;6] |> should equal None
 
@@ -266,7 +348,18 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``22 tryPick: find the first matching element, if any, and transform it`` () =
         let tryPick (p : 'a -> 'b option) (xs : 'a list) : 'b option =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353814.aspx
+           ///
+            // Does this: https://msdn.microsoft.com/en-us/library/ee353814.aspx
+            let rec func p xs l = 
+                let a::rest = xs
+                match a::rest with 
+                | a::[] -> None 
+                | _ -> 
+                     match p a with 
+                     | (Some y) -> Some y
+                     | None -> func p rest l 
+            func p xs None
+            
         let f x =
             match x<=45 with
             | true -> Some(x*2)
@@ -296,7 +389,17 @@ or something else), it's likely that you'll be able to use a fold.
         // - why can't it take an 'a->'b, instead of an 'a->'b option ?
         // - why does it return a 'b list, and not a 'b list option ?
         let choose (p : 'a -> 'b option) (xs : 'a list) : 'b list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353456.aspx
+             // Does this: https://msdn.microsoft.com/en-us/library/ee353456.aspx
+            let rec func p xs l = 
+                //let a::rest = xs
+                match xs with  
+                | a::rest -> 
+                     match p a with 
+                     | (Some y) -> func p rest (y::l)
+                     | None -> func p rest l
+                | [] -> List.rev(l)
+            func p xs []
+
         let f x =
             match x<=45 with
             | true -> Some(x*2)
@@ -314,7 +417,12 @@ or something else), it's likely that you'll be able to use a fold.
     [<Test>]
     let ``24 mapi: like map, but passes along an item index as well`` () =
         let mapi (f : int -> 'a -> 'b) (xs : 'a list) : 'b list =
-            __ // Does this: https://msdn.microsoft.com/en-us/library/ee353425.aspx
+             // Does this: https://msdn.microsoft.com/en-us/library/ee353425.aspx
+            let rec func f ind xs lst = 
+              match xs with 
+              | [] -> List.rev(lst)                    //(List.rev((f (ind) (a))::lst))
+              | a::rest -> func f (ind+1) (rest) ((f (ind) (a))::lst)
+            func f 0 xs []
         mapi (fun i x -> -i, x+1) [9;8;7;6] |> should equal [0,10; -1,9; -2,8; -3,7]
         let hailstone i t =
             match i%2 with
--- FSharpKoans/AboutStrings.fs
+++ FSharpKoans/AboutStrings.fs
@@ -6,37 +6,46 @@ module ``11: String manipulation`` =
     let ``01 Finding the length of a string`` () =
         let a = "calamari"
         let b = "It's-a me, Maaario!"
-        String.FILL_ME_IN a |> should equal 8
-        String.FILL_ME_IN b |> should equal 19
+        a.Length |> should equal 8
+        b.Length |> should equal 19
+        //String.length |> should equal 19
 
     [<Test>]
     let ``02 Getting a substring (Part 1).`` () =
         let a = "bright"
-        a.[1..] |> should equal __
+        a.[1..] |> should equal "right"
+        //a.[1..] |> should equal __
 
     [<Test>]
     let ``03 Getting a substring (Part 2).`` () =
         let a = "bright"
-        a.[..3] |> should equal __
+        a.[..3] |> should equal "brig"
+        //a.[..3] |> should equal __
 
     [<Test>]
     let ``04 Getting a substring (Part 3).`` () =
         let a = "bright"
-        a.[1..3] |> should equal __
+        a.[1..3] |> should equal "rig"
+        //a.[1..3] |> should equal __
 
     [<Test>]
     let ``05 Concatenating strings`` () =
         let a = ["hip"; "hip"; "hurray"]
-        String.FILL__ME_IN " " a |> should equal "hip hip hurray"
-        String.FILL__ME_IN __ __ |> should equal "hiphiphurray"
-        String.FILL__ME_IN __ __ |> should equal "hip! hip! hurray"
+        String.concat " " a |> should equal "hip hip hurray"
+        //String.FILL__ME_IN " " a |> should equal "hip hip hurray"
+        String.concat "" a |> should equal "hiphiphurray"
+        //String.FILL__ME_IN __ __ |> should equal "hiphiphurray"
+        String.concat "! " a |> should equal "hip! hip! hurray"
+        //String.FILL__ME_IN __ __ |> should equal "hip! hip! hurray"
 
     [<Test>]
     let ``06 Getting a string from an integer or float`` () =
         let a = 23
         let b = 17.8
-        __ a |> should equal "23"
-        __ b |> should equal "17.8"
+        string a |> should equal "23"
+        string b |> should equal "17.8"
+        //__ a |> should equal "23"
+        //__ b |> should equal "17.8"
 
     (*
         The next few tests involve the `sprintf` function, which
@@ -48,22 +57,26 @@ module ``11: String manipulation`` =
 
     [<Test>]
     let ``07 String formatting: %s format specifier`` () =
-        let result = sprintf __ "perfect"
+        let result = sprintf "%s" "Practice makes perfect."
+        //let result = sprintf __ "perfect"
         result |> should equal "Practice makes perfect."
 
     [<Test>]
     let ``08 String formatting: %d format specifier`` () =
-        let result = sprintf __ 9
+        let result = sprintf "%d %s" 9 "planets, Sir, endlessly circle, Sir"
+        //let result = sprintf __ 9
         result |> should equal "9 planets, Sir, endlessly circle, Sir"
 
     [<Test>]
     let ``09 String formatting: %b format specifier`` () =
-        let result = sprintf __ true
+        let result = sprintf "%s %b%s" "It's" true ", it is."
+        //let result = sprintf __ true
         result |> should equal "It's true, it is."
 
     [<Test>]
     let ``10 String formatting: %c format specifier`` () =
-        let result = sprintf __ 'X'
+        let result = sprintf "%c %s" 'X' "marks the spot."
+        //let result = sprintf __ 'X'
         result |> should equal "X marks the spot."
 
     // specify a precision using %.Nf, where N is an integer
@@ -71,29 +84,35 @@ module ``11: String manipulation`` =
     // The default precision is about 6, as near as I can tell.
     [<Test>]
     let ``11 String formatting: %f format specifier`` () =
-        let result = sprintf __ 2.26
-        let condensed = sprintf __ 2.26
-        let rounded = sprintf __ 2.26
+        let result = sprintf "%s %.6f%s" "Multiply by" 2.26 ", then triple"
+        //let result = sprintf __ 2.26
+        let condensed = sprintf "%s %.2f%s" "Multiply by" 2.26 ", then triple"
+        //let condensed = sprintf __ 2.26
+        let rounded = sprintf "%s %.1f%s" "Multiply by" 2.26 ", then triple"
+        //let rounded = sprintf __ 2.26
         result |> should equal "Multiply by 2.260000, then triple"
         condensed |> should equal "Multiply by 2.26, then triple"
         rounded |> should equal "Multiply by 2.3, then triple"
 
     [<Test>]
     let ``12 String formatting: %A format specifier`` () =
-        let result = sprintf __ [7.4; 7.31; 6.55]
+        let result = sprintf "%s %A %s" "Control scores:" [7.4; 7.31; 6.55] "(after transform)"
+        //let result = sprintf __ [7.4; 7.31; 6.55]
         result |> should equal "Control scores: [7.4; 7.31; 6.55] (after transform)"
-        let moreResult = sprintf __ (8,3,"UTC")
+        let moreResult = sprintf "%s %A %s" "The" (8, 3, "UTC") "time-coordinate was used."
+        //let moreResult = sprintf __ (8,3,"UTC")
         moreResult |> should equal "The (8, 3, \"UTC\") time-coordinate was used."
 
    // double-up a % to get a % in.
     [<Test>]
     let ``13 String formatting: Putting a '%' sign in`` () =
-        let result = sprintf __ 94.43
+        let result = sprintf "%s %.2f%% %s" "I scored" 94.43 "on the test"
         result |> should equal "I scored 94.43% on the test"
 
     [<Test>]
     let ``14 String formatting: Multiple format specifiers`` () =
-        let result = sprintf __ 3 5 0.6 "in other words" 60
+        let result = sprintf "%i %s %i %s %.1f%s %s%s%s %i %s" 3 "out of" 5 "is" 0.6 ", or" "(" "in other words" ")" 60 "percent."
+        //let result = sprintf __ 3 5 0.6 "in other words" 60
         result |> should equal "3 out of 5 is 0.6, or (in other words) 60 percent."
 
    // But that's not all! See the full set of formatting capabilities here:
@@ -104,10 +123,10 @@ module ``11: String manipulation`` =
     [<Test>]
     let ``15 You can use the "usual" C# string methods from F#`` () =
         let s = "  Dr Phil, PhD, MD, MC, Medicine Man  "
-        let ``first index of 'P'`` = s.FILL_ME_IN
-        let ``last index of 'P'`` = s.FILL_ME_IN
-        let ``lowercase version`` = s.FILL_ME_IN
-        let ``without surrounding space`` = s.FILL_ME_IN
+        let ``first index of 'P'`` = s.IndexOf('P')
+        let ``last index of 'P'`` = s.LastIndexOf('P')
+        let ``lowercase version`` = s.ToLower()
+        let ``without surrounding space`` = s.TrimStart().TrimEnd()
         ``first index of 'P'`` |> should equal 5
         ``last index of 'P'`` |> should equal 11
         ``lowercase version`` |> should equal "  dr phil, phd, md, mc, medicine man  "
